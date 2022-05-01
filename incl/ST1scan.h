b25a022c1e82c8ec9a8f1b7c686bf73213d6a195
PeterZhongCFS <czsxkxz@qq.com>
2022-05-01 21:39:08 +0800

    submit in main

--- incl/ST1scan.h
+++ incl/ST1scan.h
@@ -1,11 +1,32 @@
 #ifndef ST1SCAN_H
 #define ST1SCAN_H
 #include <stdio.h>
-
 void ReadST1(const char*);
+void scan()
+{
+
+}
 void ReadST1(const char*file)
 {
     FILE*FileHandle=fopen(file,"r");
-
+    
 }
+/*
+<!-- Simple -->
+<Slide>
+<Page>
+<TextBox>
+Hello,World!
+</TextBox>
+<Image height=2 width=3>
+f0 0f f0
+0f f0 0f
+</Image>
+</Page>
+<Page>
+<TextBox>
+End
+</TextBox>
+</Slide>
+*/
 #endif
--- incl/resource.h
+++ incl/resource.h
@@ -1,7 +1,13 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 typedef struct{
-    int size;
+    unsigned size;
     char*text;
+    WORD*colors;
 }TEXTBOX;
+typedef struct{
+    unsigned height;
+    unsigned width;
+    WORD**colors;
+}IMAGE
 #endif
